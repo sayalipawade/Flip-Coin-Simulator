03fc5b05444f1cd88407901ee1a6257aa89db9e5
sayalipawade <sayalipawade05@gmail.com>
2020-06-07 07:39:53 +0530

    Added:When flipping a coin showing head or tail

--- flipCoin.py
+++ flipCoin.py
@@ -1,6 +1,16 @@
 import random
 print('Welcome to flip coin Simulator')
-         
+
+#Constants
+HEAD=0
+TAIL=1
+
+#Checking Head or tail
+random_no=random.randint(0,1)
+if random_no==HEAD:
+    print("Head")
+else:
+    print("Tail")         
 
 
 
