33acdd2f96ecfb378e48f66c2f511e88bc6e4056
sayalipawade <sayalipawade05@gmail.com>
2020-06-09 11:31:52 +0530

    Refactored:Used function for user input

--- flipCoin.py
+++ flipCoin.py
@@ -1,30 +1,44 @@
 import random
 print('Welcome to flip coin Simulator')
 
-#Constants
-HEAD=0
-TAIL=1
-dict={}
-
 class Flip_coin_simulator:
+
+    #Constants
+    global HEAD
+    TAIL=1
+
+    #Variables
+    no_of_coin=0
+    no_of_time_flip=0
+
     #Checking head or tail
     def coin_flip(self,no_of_time_flip,no_of_coin):
+        HEAD=0
+        global TAIL
+        list_of_side=[]
         for index1 in range(0,no_of_time_flip):
             coin_side=""
             for index2 in range(0,no_of_coin):
                 random_no=random.randint(0,1)
-                if random_no == HEAD:
+                if random_no == HEAD: 
                     coin_side=coin_side+"H"
-                else:
+                else: 
                     coin_side=coin_side+"T"
-                dict={coin_side}
-            print(dict)
+            list_of_side.append(coin_side)
+        print(list_of_side)
 
+    #Taking user input
+    def take_user_input(self):
+        global no_of_time_flip
+        global no_of_coin
+        try:
+            no_of_coin=int(input("Enter no of coin you want to flip:"))
+            no_of_time_flip=int(input("Enter no of times you want to flip:"))
+        except ValueError:
+            print("value should be valid")
+    
 #Object creation
 flip_coin_simulator=Flip_coin_simulator()
-try:
-    no_of_coin=int(input("Enter no of coin you want to flip:"))
-    no_of_time_flip=int(input("Enter no of times you want to flip:"))
-except ValueError:
-    print("Invalid value")
+flip_coin_simulator.take_user_input()
 flip_coin_simulator.coin_flip(no_of_time_flip,no_of_coin)
+
